efc501ab6f393ced54eb8095956e0c5f06feeef9
govindpvenu <govindpvenu.txt@gmail.com>
2026-04-21 17:35:14 +0530

    feat: basic setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,15 +4,35 @@ import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const theme = NAV_THEME[colorScheme || "light"];
+
   return (
-    <ThemeProvider value={NAV_THEME[colorScheme || "light"]}>
-      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-      <Stack />
-      <PortalHost />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider value={theme}>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: theme.colors.background }}
+        >
+          <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+          <Stack
+            screenOptions={{
+              contentStyle: {
+                backgroundColor: theme.colors.background,
+              },
+              headerStyle: {
+                backgroundColor: theme.colors.background,
+              },
+              fullScreenGestureEnabled: true,
+              gestureEnabled: true,
+            }}
+          />
+          <PortalHost />
+        </SafeAreaView>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Stack } from "expo-router";
+import { Plus } from "lucide-react-native";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          title: "My Todo App",
+        }}
+      />
+      <View className="flex-1 items-center justify-start bg-background p-4">
+        <View className="flex-row items-between justify-between gap-4 mt-4 w-full  ">
+          <Input className="flex-1 rounded-full" placeholder="Enter a task" />
+          <Button className="rounded-full h-10 w-10" size="icon">
+            <Plus />
+          </Button>
+        </View>
+      </View>
+    </>
   );
 }
